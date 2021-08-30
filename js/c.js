88338393c86ad78e1e0ae7fbd254e907e014a3df
Gatash <89707192+gatash@users.noreply.github.com>
2021-08-31 01:18:42 +0300

    Add files via upload

---
+++ js/c.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#header_global").load("/includes/header_global.html");
+});
+
+$(document).ready(function(){
+    $("nav").load("/includes/nav.html");
+});
+
+$(document).ready(function(){
+    $(".cr").load("/includes/cr.html");
+});
